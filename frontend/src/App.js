315598856d96e5f6fdbec979b259fdc1588485f6
Satish Kollu <satishkollu954@gmail.com>
2025-07-17 17:12:35 +0530

    changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,17 @@ import CameraDetails from "./pages/CameraDetails";
 import CameraList from "./pages/CameraList";
 import Header from "./components/Header";
 
-function AppWrapper() {
+function AppContent() {
   const location = useLocation();
   const isLoginPage = location.pathname === "/";
-  console.log("isLoginPage", isLoginPage);
+
   return (
     <>
-      {!isLoginPage & <Header />}
+      {!isLoginPage && <Header />}
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="cameras" element={<CameraList />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cameras" element={<CameraList />} />
         <Route path="/camera/:id" element={<CameraDetails />} />
       </Routes>
     </>
@@ -26,7 +26,7 @@ function AppWrapper() {
 function App() {
   return (
     <BrowserRouter>
-      <AppWrapper />
+      <AppContent />
     </BrowserRouter>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,25 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand href="/dashboard">📹 CameraTrackPro</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/dashboard">
+          📹 CameraTrackPro
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar" />
         <Navbar.Collapse id="navbar">
           <Nav className="ms-auto">
-            <LinkContainer to="/dashboard">
-              <Nav.Link>Dashboard</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/cameras">
-              <Nav.Link>Cameras</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/">
-              <Nav.Link>Logout</Nav.Link>
-            </LinkContainer>
+            <Nav.Link as={Link} to="/dashboard">
+              Dashboard
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cameras">
+              Cameras
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Logout
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
